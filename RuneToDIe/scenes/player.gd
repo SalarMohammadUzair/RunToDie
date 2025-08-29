176f52edbbf77914d97327a0154112983f453af6
smbleh <159572574+SalarMohammadUzair@users.noreply.github.com>
2025-08-29 17:40:14 +0500

    Add files via upload
    First Commit

---
+++ RuneToDIe/scenes/player.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("left", "right")
+	if velocity != Vector2(0,0): 
+		$AnimatedSprite2D.play("default")
+		if velocity.x < 0:
+			$AnimatedSprite2D.flip_h = true
+		elif velocity.x > 0:
+			$AnimatedSprite2D.flip_h = false
+	else: 
+		$AnimatedSprite2D.stop()
+	if direction:
+		velocity.x = direction * SPEED
+		
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+
+func _on_collision_shape_2d_child_entered_tree(node):
+	$"../AudioStreamPlayer2D".play("default")
+	print('sound')# Replace with function body.
+
+
+func _on_area_2d_body_entered(body):
+	$"../AudioStreamPlayer2D".play()
+	print('sound')# Re
---
+++ RuneToDIe/scenes/world_environment.gd
@@ -0,0 +1,24 @@
+extends WorldEnvironment
+#func _ready():
+	#for is_ancestor_of(OfflineMultiplayerPeer):
+		#if x + 6 is VisualInstance3D
+		#UniformSetCacheRD @export @@export_category('yoyo honey sing')
+			#print("let this be a lesson for us my mista's")
+				#process
+#func ready(): -> void
+#void\
+#void
+#void
+#void
+
+x = 77
+print('')
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
